2eaad29b86c902300158720a310b793ff85edb11
Lucilorate <lucilorate@proton.me>
2025-03-05 13:19:54 +0100

    tiny change

--- application/src/pages/Root.tsx
+++ application/src/pages/Root.tsx
@@ -3,10 +3,10 @@ import {Outlet} from "react-router";
 export default function Root() {
     return (
         <>
-            <main>
-                <div className={"h-28 bg-amber-400"}>
-                    <p>Header</p>
-                </div>
+            <div className={"h-28 bg-amber-400"}>
+                <p>Header</p>
+            </div>
+            <main className={"container bg-blue-200 m-auto h-screen"}>
                 <Outlet/>
             </main>
         </>
