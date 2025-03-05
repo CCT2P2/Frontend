4e01cd2bef1878d72e87cb904cac9e09127e4f71
Lucilorate <lucilorate@proton.me>
2025-03-05 13:28:15 +0100

    shadcn setup

--- application/src/pages/LoginPage.tsx
+++ application/src/pages/LoginPage.tsx
@@ -1,5 +1,10 @@
+import {Button} from "../components/ui/button.tsx";
+
 export default function LoginPage() {
     return (
-        <h1 className={"text-3xl"}>Login</h1>
+        <>
+            <h1 className={"text-3xl"}>Login</h1>
+            <Button>Waow</Button>
+        </>
     )
 }
