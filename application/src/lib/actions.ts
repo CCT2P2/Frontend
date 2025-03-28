f7e92bb689bbb9d8c5a58113ef1e77c9bd63a902
xLTJ <lucilorate@proton.me>
2025-03-28 02:00:24 +0100

    Improved validation, improved form, cleanup
    Added error messages for remaining fields, added check for matching password and confirm password, fixed checkboxes not being required, made RegisterForm a lot cleaner by making the inputs into seperate components (idk why i hadnt done this before), made some comments in the createAccount action to make it easier to understand whats happening.

--- application/src/lib/actions.ts
+++ application/src/lib/actions.ts
@@ -16,6 +16,7 @@ export async function signIn(prevState: string | undefined, formData: FormData) 
 
 // Create account -------------------------------------------------------------
 
+// Schema to validate fields based on. Message is the error message shown to the user if the requirement is not met
 const CreateAccountSchema = z.object({
     username: z
         .string()
@@ -29,43 +30,70 @@ const CreateAccountSchema = z.object({
 
     password: z
         .string()
-        .min(6, {message: 'Password must be at least 6 characters'})
-});
+        .min(6, {message: 'Password must be at least 6 characters'}),
 
+    confirmPassword: z
+        .string(),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords must match',
+    path: ["confirmPassword"]
+})
+
+// State for the signup form (and basically all form states so im only writing a super long explanation for this one)
+// - errors are the error message(s) shown to the user.
+// - fieldState is the state of the fields before the form is submitted, used to keep these values in the form in case
+// of an invalid input so the user doesnt have to type it again.
+// - message is the general error message, not shown to the user (just at the moment, we should probably show this too)
+// - userId is the user id of the created user, which is returned if the registration succeeds. no idea if we
+// actually need this, but i just included it for now lol
 export interface SignupState {
-    error?: {
+    errors?: {
         username?: string[],
         email?: string[],
         password?: string[],
+        confirmPassword?: string[],
     },
+    fieldsState?: {
+        username?: string,
+        email?: string,
+        password?: string,
+        confirmPassword?: string,
+    }
     message?: string | null,
-    userId?: null,
+    userId?: number,
 }
 
 export async function createAccount(_prevState: SignupState, formData: FormData): Promise<SignupState> {
-    for (const [key, value] of formData.entries()) {
-        console.log(key, value)
-    }
-
+    // uses the validationSchema to parse the data from the form.
     const validatedField = CreateAccountSchema.safeParse({
         username: formData.get('username'),
         email: formData.get('email'),
         password: formData.get('password'),
+        confirmPassword: formData.get('confirmPassword'),
     });
 
+    // if input isnt return error messages and keep field states
     if (!validatedField.success) {
+        const fields: Record<string, string> = {}
+        for (const [key, value] of formData.entries()) {
+            fields[key] = value.toString()
+        }
+
         return {
             errors: validatedField.error.flatten().fieldErrors,
+            fieldsState: fields,
             message: 'Missing or invalid fields, failed to create account'
         }
     }
 
+    // gets the data required for the request ready
     const requestData: UserRegistrationRequest = {
         username: validatedField.data.username,
         email: validatedField.data.email,
         password: validatedField.data.password,
     }
 
+    // post request to backend api to create new account
     const response = await fetch('/api/auth/register', {
         method: 'POST',
         headers: {
@@ -74,12 +102,14 @@ export async function createAccount(_prevState: SignupState, formData: FormData)
         body: JSON.stringify(requestData),
     });
 
+    // any other status than 200 means there was an error, so it gets returned
     if (response.status !== 200) {
         return {
             message: `Error creating account: ${response.status}`
         }
     }
 
+    // converts response to json and returns user id
     const responseData: UserRegistrationResponse = await response.json()
     return {userId: responseData.user_id}
 }
