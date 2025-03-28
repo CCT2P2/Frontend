68961da1d124ed7c6cd4226bfd8dede53f34055d
xLTJ <lucilorate@proton.me>
2025-03-28 15:25:47 +0100

    fixed config

--- application/vite.config.ts
+++ application/vite.config.ts
@@ -4,19 +4,21 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: "http://localhost:5000",
-                changeOrigin: true,
-                secure: false,
+        plugins: [react()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
             }
         },
-        allowedHosts: ["gnuf.online"],
+        server: {
+            proxy: {
+                '/api': {
+                    target: "http://localhost:5000",
+                    changeOrigin: true,
+                    secure: false,
+                }
+            },
+            allowedHosts: ["gnuf.online"],
+        }
     }
-})
+)
