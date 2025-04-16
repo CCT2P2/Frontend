0b9e7b9c32e537ea6939fc3406eb0faff010ce05
Stinnia <32062581+Stinnia@users.noreply.github.com>
2025-04-16 12:39:33 +0200

    User settings added

--- application/components/ui/button.tsx
+++ application/components/ui/button.tsx
@@ -1,9 +1,9 @@
 import * as React from "react"
-import {Slot} from "@radix-ui/react-slot"
-import {cva, type VariantProps} from "class-variance-authority"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
 
-import {cn} from "@/lib/utils"
-import {useUISettings} from "@/app/store/useUISettings";
+import { cn } from "@/lib/utils"
+import { useUISettings } from "@/app/store/useUISettings";
 
 const buttonVariants = cva(
     "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm" +
@@ -27,6 +27,7 @@ const buttonVariants = cva(
                 ghost:
                     "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 rounded-xl",
                 link: "text-primary underline-offset-4 hover:underline",
+                delete: "text-red-500 font-semibold hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 rounded-xl ",
             },
             size: {
                 default: "h-9 px-4 py-2 has-[>svg]:px-3",
@@ -43,23 +44,23 @@ const buttonVariants = cva(
 )
 
 function Button({
-                    className,
-                    variant,
-                    size,
-                    asChild = false,
-                    ...props
-                }: React.ComponentProps<"button"> &
+    className,
+    variant,
+    size,
+    asChild = false,
+    ...props
+}: React.ComponentProps<"button"> &
     VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-}) {
-    const {blur} = useUISettings();
+        asChild?: boolean
+    }) {
+    const { blur } = useUISettings();
     const Comp = asChild ? Slot : "button"
 
     return (
         <Comp
             data-slot="button"
             className={cn(
-                buttonVariants({variant, size, className}),
+                buttonVariants({ variant, size, className }),
                 blur && variant !== "ghost" ? "bg-stone-800/10 backdrop-blur-md" : "bg-black",
                 variant === "ghost" && "bg-transparent"
             )}
@@ -68,4 +69,4 @@ function Button({
     )
 }
 
-export {Button, buttonVariants}
+export { Button, buttonVariants }
