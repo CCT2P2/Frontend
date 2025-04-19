130888551b24db0b03643b56eae477f606dd9cda
Sebastian Lindau-Skands <sebastian.lindauskands@proton.me>
2025-04-19 16:50:49 +0200

    made forum lists dynamic

--- application/components/forumPage/ForumList.tsx
+++ application/components/forumPage/ForumList.tsx
@@ -1,27 +1,44 @@
 "use client";
-import {Card, CardTitle} from "@/components/ui/card";
-import {useUISettings} from "@/app/store/useUISettings";
-import {Link} from "lucide-react";
+import { Card, CardTitle } from "@/components/ui/card";
+import { useUISettings } from "@/app/store/useUISettings";
+import { Link } from "lucide-react";
+import { useAuthFetch } from "@/lib/hooks/useAuthFetch";
+import { GetAllCommunitiesResponse } from "@/lib/apiTypes";
+import { useState } from "react";
 
 export default function ForumList() {
-    const {paddingButton, padding} = useUISettings();
-    return (
-        <div className="h-screen flex flex-col gap-3">
+  const {
+    data: forums,
+    isLoading,
+    status,
+    error,
+  } = useAuthFetch<GetAllCommunitiesResponse>(`/api/community/all`); //todo: replace with user's communities instead of all
+  const [communityName, setCommunityName] = useState("");
+  const [communityDescription, setCommunityDescription] = useState("");
+  const { paddingButton, padding } = useUISettings();
+  const filteredForums = forums?.slice(2);
+  return (
+    <div className="h-screen flex flex-col gap-3">
+      <Card
+        className={`p-${padding} border-secondary gap-3 overflow-y-auto max-h-[80%]`}
+      >
+        <CardTitle>Forums</CardTitle>
+        {forums ? (
+          forums.map((forum) => (
             <Card
-                className={`p-${padding} border-secondary gap-3 overflow-y-auto max-h-[80%]`}
+              key={forum.communityID}
+              className={`p-${paddingButton} transition-colors duration-200 hover:bg-secondary/15`}
+              onClick={() =>
+                (window.location.href = `/forum/${forum.communityID}`)
+              }
             >
-                <CardTitle>Forums</CardTitle>
-                <Card
-                    className={`p-${paddingButton} transition-colors duration-200 hover:bg-secondary/15`}
-                >
-                    <Link href="/forum/-1">The realm of the bannished</Link>
-                </Card>
-                <Card
-                    className={`p-${paddingButton} transition-colors duration-200 hover:bg-secondary/15`}
-                >
-                    <Link href="/forum/3">C#</Link>
-                </Card>
+              <div>{forum.names}</div>
             </Card>
-        </div>
-    );
+          ))
+        ) : (
+          <p>Loading forums...</p>
+        )}
+      </Card>
+    </div>
+  );
 }
