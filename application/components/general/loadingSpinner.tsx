8522132f9873bad5d25dd45932a8db4951621b71
xLTJ <lucilorate@proton.me>
2025-04-20 14:24:39 +0200

    improved loading spinner

--- application/components/general/loadingSpinner.tsx
+++ application/components/general/loadingSpinner.tsx
@@ -1,10 +1,23 @@
 import {cn} from "@/lib/utils";
+import {useEffect, useState} from "react";
 
 interface Props {
     absolute?: boolean;
+    delay?: number;
 }
 
-export default function LoadingSpinner({absolute = true}: Props) {
+export default function LoadingSpinner({absolute = true, delay = 0}: Props) {
+    const [showSpinner, setShowSpinner] = useState(false);
+
+    useEffect(() => {
+        const timeout: NodeJS.Timeout = setTimeout(() => {
+            setShowSpinner(true);
+        }, delay);
+        return () => clearTimeout(timeout);
+    }, [delay]);
+
+    if (!showSpinner) return null;
+
     return (
         <span className={cn(
             "inset-[50%] loading loading-spinner loading-lg",
